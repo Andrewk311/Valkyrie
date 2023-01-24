4b0a3c1fa60373ec38367586666e1f0c6fee0538
Andrewk311 <Andrewking0619@gmail.com>
2023-01-23 19:54:32 -0500

    start

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Valkyrie!</Text>
+      <Image source={require('./drone.png')} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
