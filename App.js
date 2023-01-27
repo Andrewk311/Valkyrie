51364d77745ace477135de9873b9fa01d3fa9c88
Andrewk311 <Andrewking0619@gmail.com>
2023-01-27 12:12:12 -0500

    testing navigation changes

--- App.js
+++ App.js
@@ -3,9 +3,35 @@ import { Button } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native';
 import { withAuthenticator } from 'aws-amplify-react-native'
-
+import HomeScreen from "./src/screens/HomeScreen";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 import Amplify from '@aws-amplify/core'
 import config from './src/aws-exports'
+
+const AppNavigator = createStackNavigator(
+  {
+    Home: HomeScreen,
+  },
+  {
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#006600",
+      },
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: "#FFF",
+      },
+      headerTintColor: "#FFF",
+    },
+  },
+  {
+    initialRouteName: "Home",
+  }
+);
+
+const Navigator = createAppContainer(AppNavigator);
+
 Amplify.configure(config)
 
 function SignOutButton() {
@@ -24,9 +50,9 @@ export default function App() {
           "phone_number",
         ]}
       >
-        <View style={style.container}>
-          <SignOutButton  />
-        </View>
+        <Navigator>
+          <HomeScreen />
+        </Navigator>
       </Authenticator>
     </Authenticator.Provider>
   );
@@ -34,5 +60,5 @@ export default function App() {
 
 const style = StyleSheet.create({
   container: { flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#E33737'},
-  button: {},
+
 });
