0184a6c9b9094f0e7cf75d99343d209e7667285d
Andrewk311 <Andrewking0619@gmail.com>
2023-02-02 18:34:15 -0500

    added some styling to tracking

--- src/screens/homeScreens/TrackingScreen.js
+++ src/screens/homeScreens/TrackingScreen.js
@@ -2,20 +2,23 @@ import React from "react";
 import { Text, View, Button, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import MapView from 'react-native-maps';
+import { Dimensions } from "react-native";
   
 const Tracking = (props) => {
     return (
-      
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-
-        <Text style={{ color: "#006600", fontSize: 40 }}>Tracking Screen!</Text>
-        <Ionicons name="ios-map-sharp" size={80} color="#006600" />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: "rgba(227,55,55,1)", flex: 1}}>
+        <View style={styles.rect}>
+          <Text style={styles.where}> Where's My Order? </Text>
+        </View>
         <MapView style={styles.map} initialRegion={{
           latitude: 40.499046325683594,
           longitude: -74.4476089477539,
           latitudeDelta: 0.005,
           longitudeDelta: 0.005,}} 
         />
+        {/* <View style={styles.rect2}>
+
+        </View> */}
         
       </View>
     );
@@ -45,5 +48,31 @@ const styles = StyleSheet.create({
     width: '90%',
     height: '60%',
     borderColor: '#FF0000',
+    paddingHorizontal: 50,
+    paddingVertical:40,
+    marginTop: 100
   },
+  rect: {
+    width: Dimensions.get('window').width,
+    height: 69,
+    alignSelf: "center",
+    backgroundColor: "#FFFFFF",
+    marginTop: -117
+  },
+  where: {
+    color: "#AA9798",
+    height: 47,
+    width: 375,
+    fontSize: 40,
+    textAlign: "center",
+    fontWeight:"bold", 
+    marginTop: 20
+  }, 
+  // rect2: {
+  //   width: Dimensions.get('window').width,
+  //   height: 69,
+  //   alignSelf: "center",
+  //   backgroundColor: "#FFFFFF",
+  //   marginTop: 138
+  // },
 });
