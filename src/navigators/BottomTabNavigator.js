09b57f00e2f7e77cd269f00e2775b19b67d76a10
adurasno <durasno.ayleen@gmail.com>
2023-02-02 16:23:10 -0500

    BottomnNavBar styling

--- src/navigators/BottomTabNavigator.js
+++ src/navigators/BottomTabNavigator.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import CustomNavigationAccount from './CustomNavigationAccount';
 import CustomNavigationBrowse from './CustomNavigationBrowse';
 import CustomNavigationHome from './CustomNavigationHome';
+import { Dimensions } from 'react-native';
 
 const Tab = createMaterialBottomTabNavigator();
 
@@ -11,24 +13,43 @@ function MyTabs() {
     return (
         <Tab.Navigator
             initialRouteName="Home"
-            activeColor="#FFFFFF"
+            activeColor="#000000"
             inactiveColor="#FFFFFF"
-            barStyle={{ backgroundColor: '#AA9798' }}
+            barStyle={{ 
+                backgroundColor: '#AA9798',
+                height:Dimensions.get('window').height*(0.1),
+                alignContent:"center", 
+                justifyContent:"center",
+            }}
         >
             <Tab.Screen
                 name="Home"
                 component={CustomNavigationHome}
-                options={{ tabBarLabel: "Home" }}
+                options={{ 
+                    tabBarLabel:"Home",
+                    tabBarIcon: (tabInfo) => (
+                        <Ionicons name="ios-home" size={30} />
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Browse"
                 component={CustomNavigationBrowse}
-                options={{ tabBarLabel: "Browse" }}
+                options={{ 
+                    tabBarLabel: "Browse",
+                    tabBarIcon: (tabInfo) => (
+                        <Ionicons name="ios-cart" size={50} resizeMode='contain'/>
+                    ),
+                }}
             />
             <Tab.Screen
                 name="Account"
                 component={CustomNavigationAccount}
-                options={{ tabBarLabel: "Account" }}
+                options={{ 
+                    tabBarLabel: "Account",
+                    tabBarIcon: (tabInfo) => (
+                        <Ionicons name="ios-person" size={30} resizeMode='contain'/>
+                    ), }}
             />
 
         </Tab.Navigator>
