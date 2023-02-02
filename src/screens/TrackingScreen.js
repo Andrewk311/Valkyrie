404e97d8a69f46def4132929294ca4b0a7c01c37
Andrew King <Andrewking0619@gmail.com>
2023-02-02 10:23:32 -0500

    added some map thing

--- src/screens/TrackingScreen.js
+++ src/screens/TrackingScreen.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
+import { Text, View, Button, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import MapView from 'react-native-maps';
   
 const Tracking = (props) => {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Text style={{ color: "#006600", fontSize: 40 }}>Tracking Screen!</Text>
         <Ionicons name="ios-map-sharp" size={80} color="#006600" />
+        <MapView style={styles.map} />
       </View>
     );
   };
@@ -26,3 +28,13 @@ Tracking.navigationOptions = ({ navigation: { goBack } }) => {
   }
 
 export default Tracking;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
