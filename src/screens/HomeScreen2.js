641ac131881e5421767e83122d7888465f464f94
adurasno <durasno.ayleen@gmail.com>
2023-02-01 20:38:39 -0500

    Containers for Home

--- src/screens/HomeScreen2.js
+++ src/screens/HomeScreen2.js
@@ -23,6 +23,41 @@ const Home = (props) => {
                 <Text style={{color:"#92989B"}}>500 Hadley Center Dr, {"\n"}South Plainfield, NJ, 08901</Text>
                 </Text>
             </View>
+          <View style={styles.ad}>
+            <Text style={styles.blackBold}>We've got you covered</Text>
+            <Text style={{color:"#92989B"}}>Schedule a Delivery right to your door step</Text>
+          </View>
+          <View>
+            <Text style={{fontWeight:"bold", color:"#FFFFFF", fontSize:39, marginLeft:10, marginTop:10}}>Popular</Text>
+          </View>
+          <View style={styles.widgetContainer}>
+            <TouchableOpacity
+                style={styles.widget}
+                onPress={() => navigate('Messages')} 
+              >
+                <Ionicons name="mail" size={20} color="#000000" alignSelf="center" />
+                <Text style={styles.customBtnText}>Refill a Prescription</Text>
+            </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.widget}
+                onPress={() => navigate('Messages')} 
+              >
+                <Ionicons name="mail" size={20} color="#000000" alignSelf="center" />
+                <Text style={styles.customBtnText}>Contact a Pharmacist</Text>
+            </TouchableOpacity>
+
+              <TouchableOpacity
+                style={styles.widget}
+                onPress={() => navigate('Messages')} 
+              >
+                <Ionicons name="mail" size={20} color="#000000" alignSelf="center" />
+                <Text style={styles.customBtnText}>Track Your Order</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.ad2}>
+              <Text style={styles.blackBold}>Selected Pharmacy</Text>
+            </View>
           </View>
         </Authenticator>
     </Authenticator.Provider>
@@ -109,7 +144,10 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width,
     height: 124,
     backgroundColor: "rgba(255,255,255,1)",
-    marginTop: 24
+    marginTop: 10,
+    borderRadius: 15,
+    width:win.width*(0.98),
+    alignSelf:"center"
   },
   blackBold: {
     color: "#00000",
@@ -138,6 +176,38 @@ const styles = StyleSheet.create({
   customBtnText: {
     alignContent:"center",
     marginLeft:2,
-  }
+  },
+  ad: {
+    borderRadius: 15,
+    backgroundColor: "white",
+    height: win.height*(0.15),
+    width:win.width*(0.98),
+    alignSelf:"center",
+    marginTop:10,
+  },
+  widgetContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent:"center",
+    marginTop:10,
+    alignContent:"center",
+  },
+  widget: {
+    backgroundColor: "white", 
+    width: win.width/3.5, 
+    borderRadius: 30,
+    marginLeft:10,
+    marginRight:10
+  },
+  ad2: {
+    borderRadius: 15,
+    backgroundColor: "white",
+    height:win.height*(0.2),
+    width:win.width*(0.98),
+    alignSelf:"center",
+    marginTop:10,
+    marginBottom:10
+  },
   
 });
