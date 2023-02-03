c6a1e98bae3d9ebbd3c6b5e72f568e4dbbcc9f94
adurasno <durasno.ayleen@gmail.com>
2023-02-03 16:53:01 -0500

    Browser Screen

--- src/screens/BrowseScreen.js
+++ src/screens/BrowseScreen.js
@@ -1,48 +1,204 @@
-import React, { useState } from "react";
-import { Text, View, TextInput, Button } from "react-native";
+import { useState } from "react";
+import { Text, View, TextInput, Button, Dimensions, TouchableOpacity, Image} from "react-native";
+import { ScrollView, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Searchbar } from 'react-native-paper';
 import { Ionicons } from "@expo/vector-icons";
 
 const Browse = (props) => {
   const [input, setInput] = useState("");
+  const [searchQuery, setSearchQuery] = React.useState('');
+  const onChangeSearch = query => setSearchQuery(query);
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: "#006600", fontSize: 40 }}>Browse Screen!</Text>
-      <Ionicons name="ios-home" size={80} color="#006600" />
-      <TextInput
-        placeholder="Enter your name"
-        value={input}
-        onChangeText={(value) => setInput(value)}
-      />
-      <Button
-        title="Go to Cart Screen"
-        color="#006600"
-        onPress={() => props.navigation.navigate("Cart", { username: input })}
-      />
+    <ScrollView style={{marginHorizontal:0, backgroundColor:"rgba(227,55,55,1)"}}>
+      <View style={{backgroundColor:'white'}}>
+      <Searchbar
+      placeholder="Search..."
+      onChangeText={onChangeSearch}
+      value={searchQuery}
+      platform={Platform.OS}
+    />
     </View>
+      <View style={styles.rowContainer}>
+        <View style={styles.column1}>
+          <TouchableOpacity
+                  style={styles.category}
+                  onPress={() => props.navigation.navigate('Settings')} 
+                >         
+                <View>     
+                <Text style={styles.optionTitle}>COVID-19{"\n"}Tests</Text>
+                </View>  
+                <Image style={styles.tinyLogo}
+              source={require('./../customIcons/Covid.png')}
+              />
+              </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.category}
+                onPress={() => props.navigation.navigate('Settings')} 
+              >         
+              <View>     
+              <Text style={styles.optionTitle}>Immune{"\n"}Support</Text>
+              </View>  
+              <Image style={styles.tinyLogo}
+              source={require('./../customIcons/Pill.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.category}
+                onPress={() => props.navigation.navigate('Settings')} 
+              >         
+              <View>     
+              <Text style={styles.optionTitle}>Flu/Cold{"\n"}Medicine</Text>
+              </View>  
+              <Image style={styles.tinyLogo}
+              source={require('./../customIcons/Medicine.png')}
+              />
+            </TouchableOpacity>
+        </View>
+        <View style={styles.columns}>
+        <TouchableOpacity
+                  style={styles.category}
+                  onPress={() => props.navigation.navigate('Settings')} 
+                >         
+                <View>     
+                <Text style={styles.optionTitle}>First-Aid{"\n"}Care</Text>
+                </View>
+                <Image style={styles.tinyLogo}
+              source={require('./../customIcons/FirstAid.png')}
+              />
+              </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.category}
+                onPress={() => props.navigation.navigate('Settings')} 
+              >         
+              <View>     
+              <Text style={styles.optionTitle}>Sanitation Supplies</Text>
+              </View>  
+              <Image style={styles.tinyLogo}
+              source={require('./../customIcons/Sanitize.png')}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.category}
+                onPress={() => props.navigation.navigate('Settings')} 
+              >         
+              <View>     
+              <Text style={styles.optionTitle}>Pharmacy{"\n"}</Text>
+              </View>  
+              <Image style={styles.tinyLogo}
+              source={require('./../customIcons/RX.png')}
+              />
+            </TouchableOpacity>
+          </View>
+      </View>
+    </ScrollView>
   );
 };
 
 Browse.navigationOptions = ({ navigation }) => {
   const { navigate } = navigation
   return  {
-    title: 'Browse',
+    title:"VALKYRIE",
+    headerLeft: () => (
+      <Image
+      style={{width: 55, marginTop:-8, marginLeft:3, resizeMode: 'contain'}}
+      source={require('../../assets/logo.png')} 
+      ></Image>
+    ),
+
     headerRight: () => (
-      <>
-      <Button 
-        title="About Us" backgroundColor="rgba(0,0,0,0)" color="rgba(0,122,255,1)"
-        onPress={() => navigate('About')}
-      />
-      <Button 
-        title="Messages" backgroundColor="rgba(0,0,0,0)" color="rgba(0,122,255,1)"
-        onPress={() => navigate('Messages')}
-      />
-      <Button 
-        title="Cart" backgroundColor="rgba(0,0,0,0)" color="rgba(0,122,255,1)"
-        onPress={() => navigate('Cart')}
-      />
-      </>
+    <>
+      <View style={styles.container1}>
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            style={styles.customBtn}
+            onPress={() => navigate('About')} 
+          >
+            <Ionicons name="people-sharp" size={20} color="#000000" />
+            <Text style={styles.customBtnText}>About Us</Text>
+         </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+        <TouchableOpacity
+            style={styles.customBtn}
+            onPress={() => navigate('Messages')} 
+          >
+            <Ionicons name="mail" size={20} color="#000000"/>
+            <Text style={styles.customBtnText}>Messages</Text>
+         </TouchableOpacity>
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.customBtn}
+            onPress={() => navigate('Cart')} 
+          >
+            <Ionicons name="cart" size={20} color="#000000"/>
+            <Text style={styles.customBtnText}>Cart</Text>
+         </TouchableOpacity>
+        </View>
+
+      </View>
+        </>
     )
   }             
 }
 
 export default Browse;
+const styles = StyleSheet.create({
+  rowContainer:{
+    flexDirection: 'row',
+    marginTop:0,
+    alignContent:'center',
+    justifyContent:'center'
+  },
+  column1:{
+    flexDirection: 'column',
+    alignContent:'center',
+    marginRight:15
+  },
+  category: {
+    backgroundColor: "white",
+    borderRadius:12,
+    width:Dimensions.get('window').width*(0.98)*(0.45),
+    height:Dimensions.get('window').width*(0.37),
+    marginTop:14,
+    flexDirection: 'column',
+  },
+  optionTitle:{
+    fontWeight:"bold",
+    fontSize:23,
+    color:"black",
+    marginLeft:-20,
+    alignSelf:'center',
+    marginTop:20
+  },
+  tinyLogo:{
+    width: 40,
+    height: 40,
+    resizeMode:'contain',
+    marginLeft:Dimensions.get('window').width*(0.98)*(0.45) -55,
+  },
+  container1: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginRight:10
+  },
+  buttonContainer: {
+    flex: 1,
+    alignContent:"center"
+  },
+  customBtn:{
+    flex: 1,
+    flexDirection: "column",
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingLeft: 8
+  },
+  customBtnText: {
+    alignContent:"center",
+    marginLeft:2,
+  },
+});
