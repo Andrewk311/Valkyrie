5f7deba570585b9b2a0cf1b5659f9de5aa9396aa
Andrewk311 <Andrewking0619@gmail.com>
2023-02-20 22:51:09 -0500

    initialize website

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import dog from './Dallas.JPG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>
+          Valkyrie Drone Landing Page!
+        </p>
+        <img src={dog} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
