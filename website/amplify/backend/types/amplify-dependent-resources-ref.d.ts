fd87c24dec4ad01131910b0bb17535cdc962474f
Andrewk311 <Andrewking0619@gmail.com>
2023-02-20 22:53:32 -0500

    initialized amplify

---
+++ website/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,14 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "valkyrie9d01b9df": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
