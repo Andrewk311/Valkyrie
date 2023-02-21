1ada8a358716d5a2491746f548291347fcd01e08
Andrewk311 <Andrewking0619@gmail.com>
2023-02-21 17:53:53 -0500

    fixed routing

--- website/src/App.js
+++ website/src/App.js
@@ -1,18 +1,17 @@
 import dog from './Dallas.JPG';
 import './App.css';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <p>
-          Valkyrie Drone Landing Page!
-        </p>
-        <Link to="https://pharmacy.valkyriedrone.io">
-          <button>Go to Pharm portal</button>
-        </Link>
-        <img src={dog} />
+        Valkyrie Drone Landing Page!
+      </p>
+      <nav>
+        <a href="https://pharmacy.valkyriedrone.io"> Pharm portal</a>
+      </nav>
+      <img src={dog} />
       </header>
     </div>
   );
