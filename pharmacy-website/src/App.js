722e9c453cd3336a0719680bd109436fea193961
Andrewk311 <Andrewking0619@gmail.com>
2023-02-21 19:04:50 -0500

    added auth

--- pharmacy-website/src/App.js
+++ pharmacy-website/src/App.js
@@ -1,19 +1,36 @@
 import dog from './dallas2.JPG';
 import './App.css';
+import { withAuthenticator, Button, Heading, Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import Amplify from '@aws-amplify/core';
+import config from './aws-exports'
+
+Amplify.configure(config)
+
+function SignOutButton() {
+  const { signOut } = useAuthenticator();
+  return <Button width='100px' height='100px' color='#000000' title="Sign Out" onPress={signOut} />;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          PHARM portal
-        </p>
-        <nav>
-          <a href="https://www.valkyriedrone.io"> Back to landing page</a>
-        </nav>
-        <img src={dog}/>
-      </header>
-    </div>
+    <Authenticator.Provider>
+      <Authenticator>
+        <div className="App">
+          <header className="App-header">
+          
+            <p>
+              PHARM portal
+            </p>
+            <SignOutButton/>
+            <nav>
+              <a href="https://www.valkyriedrone.io"> Back to landing page</a>
+            </nav>
+            
+            <img src={dog}/>
+          </header>
+        </div>
+      </Authenticator>
+    </Authenticator.Provider>
   );
 }
 
