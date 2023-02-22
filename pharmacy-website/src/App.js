2dc4f7e65e00e7c9ab51479b0c731dbfc517aba2
Andrewk311 <Andrewking0619@gmail.com>
2023-02-21 19:34:38 -0500

    auth signout working

--- pharmacy-website/src/App.js
+++ pharmacy-website/src/App.js
@@ -1,16 +1,28 @@
 import dog from './dallas2.JPG';
 import './App.css';
-import { withAuthenticator, Button, Heading, Authenticator, useAuthenticator } from '@aws-amplify/ui-react';
+import {Auth} from 'aws-amplify';
+import { withAuthenticator, Button, Heading, Authenticator, useAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import Amplify from '@aws-amplify/core';
 import config from './aws-exports'
 
 Amplify.configure(config)
 
 function SignOutButton() {
+  console.log('working');
   const { signOut } = useAuthenticator();
   return <Button width='100px' height='100px' color='#000000' title="Sign Out" onPress={signOut} />;
 }
 
+// signOut = () => {
+//   Auth.signOut()
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+// }
+const signOut = () => {
+  Auth.signOut()
+    .then(() => console.log('Signed out'))
+    .catch(err => console.log('Error signing out: ', err));
+}
 function App() {
   return (
     <Authenticator.Provider>
@@ -21,11 +33,10 @@ function App() {
             <p>
               PHARM portal
             </p>
-            <SignOutButton/>
             <nav>
               <a href="https://www.valkyriedrone.io"> Back to landing page</a>
             </nav>
-            
+            <button onClick={signOut} className="signOutButton">SignOut</button>
             <img src={dog}/>
           </header>
         </div>
@@ -34,4 +45,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App,true);
