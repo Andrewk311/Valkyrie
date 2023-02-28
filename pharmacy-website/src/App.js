32d1503a37185ac826466b089c63aadf92c127b4
Andrewk311 <Andrewking0619@gmail.com>
2023-02-28 01:00:58 -0500

    attempted to add auth styling,got rid of signup bar. ThemeProvider is temporary and does not work even though according to the documentation it should.

--- pharmacy-website/src/App.js
+++ pharmacy-website/src/App.js
@@ -1,6 +1,6 @@
 import dog from './dallas2.JPG';
 import './App.css';
-import {Authenticator} from '@aws-amplify/ui-react';
+import {Authenticator, Theme, ThemeProvider} from '@aws-amplify/ui-react';
 import Amplify from '@aws-amplify/core';
 import config from './aws-exports'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -10,19 +10,53 @@ import Home from './screens/Home'
 
 Amplify.configure(config)
 
+const theme = {
+  name: 'My Custom Theme',
+  tokens: {
+    colors: {
+      primary: '#007bff',
+      secondary: '#6c757d',
+    },
+    components: {
+      button: {
+        fontWeight: 'bold',
+        borderRadius: '9999px',
+        padding: '10px 20px',
+        borderWidth: '2px',
+        borderStyle: 'solid',
+        borderColor: {
+          value: 'var(--amplify-colors-primary)',
+        },
+        backgroundColor: {
+          value: 'var(--amplify-colors-primary)',
+        },
+        color: {
+          value: 'var(--amplify-colors-white)',
+        },
+        _hover: {
+          backgroundColor: {
+            value: 'var(--amplify-colors-primary-800)',
+          },
+        },
+        _focus: {
+          backgroundColor: {
+            value: 'var(--amplify-colors-primary-800)',
+          },
+        },
+      },
+    },
+  },
+};
+
 function App() {
   return (
-    <Authenticator.Provider>
-      <Authenticator>
-        <BrowserRouter>
-          <Routes>
-            <Route path ="/" element={<Home />} />
-            <Route path ="/orders" element={<Orders />} />
-            <Route path ="/inventory" element={<Inventory />} />
-          </Routes>
-        </BrowserRouter>
-      </Authenticator>
-    </Authenticator.Provider>
+    <BrowserRouter>
+      <Routes>
+        <Route path ="/" element={<Home />} />
+        <Route path ="/orders" element={<Orders />} />
+        <Route path ="/inventory" element={<Inventory />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- pharmacy-website/src/screens/Home.js
+++ pharmacy-website/src/screens/Home.js
@@ -1,8 +1,21 @@
 import dog from './../dallas2.JPG';
 import './Home.css';
 import {Auth} from 'aws-amplify';
-import { withAuthenticator, Button, Authenticator, useAuthenticator, ScrollView } from '@aws-amplify/ui-react';
+import { withAuthenticator, Button, Authenticator, useAuthenticator, ScrollView, SignIn, AmplifyAuthenticator, Theme, ThemeProvider, useTheme } from '@aws-amplify/ui-react';
 import {useNavigate} from 'react-router-dom';
+
+
+
+// const MyTheme = Object.assign({}, AmplifyTheme, {
+//     button: {
+//       ...AmplifyTheme.button,
+//       backgroundColor: 'green',
+//       borderRadius: '5px',
+//       padding: '10px 20px',
+//       fontWeight: 'bold',
+//       cursor: 'pointer'
+//     }
+//   });
 
 const signOut = () => {
   Auth.signOut()
@@ -11,6 +24,57 @@ const signOut = () => {
 }
 
 function App() {
+
+    const { tokens } = useTheme();
+  const theme = {
+    name: 'Auth Example Theme',
+    tokens: {
+      colors: {
+        background: {
+          primary: {
+            value: tokens.colors.neutral['90'].value,
+          },
+          secondary: {
+            value: tokens.colors.neutral['100'].value,
+          },
+        },
+        font: {
+          interactive: {
+            value: tokens.colors.white.value,
+          },
+        },
+        brand: {
+          primary: {
+            '10': tokens.colors.teal['100'],
+            '80': tokens.colors.teal['40'],
+            '90': tokens.colors.teal['20'],
+            '100': tokens.colors.teal['10'],
+          },
+        },
+      },
+      components: {
+        tabs: {
+          item: {
+            _focus: {
+              color: {
+                value: tokens.colors.white.value,
+              },
+            },
+            _hover: {
+              color: {
+                value: tokens.colors.yellow['80'].value,
+              },
+            },
+            _active: {
+              color: {
+                value: tokens.colors.white.value,
+              },
+            },
+          },
+        },
+      },
+    },
+  };
 
   const navigate = useNavigate();
 
@@ -23,8 +87,8 @@ function App() {
   }
 
   return (
-    <Authenticator.Provider>
-      <Authenticator>
+    <ThemeProvider theme={theme}>
+    <Authenticator>
         <div className="App">
             <div className='App-header'>
                 <p style={{ color:'#92989B', fontWeight: 'bold', whiteSpace:'pre-line', textAlign:'center'}}>VALKYRIE{"\n"}
@@ -36,8 +100,8 @@ function App() {
                 <button onClick={handleInventoryClick} className="button">View Inventory</button>
                 <button onClick={signOut} className="button">Sign Out</button>
             </div>
-      </Authenticator>
-    </Authenticator.Provider>
+    </Authenticator>
+    </ThemeProvider>
   );
 }
 
