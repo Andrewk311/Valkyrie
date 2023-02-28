e4e34e1051f6b48c493d6425a013923e44c34c98
Andrewk311 <Andrewking0619@gmail.com>
2023-02-28 16:13:29 -0500

    fixed small text thing

--- pharmacy-website/src/screens/Home.js
+++ pharmacy-website/src/screens/Home.js
@@ -92,7 +92,7 @@ function App() {
         <div className="App">
             <div className='App-header'>
                 <p style={{ color:'#92989B', fontWeight: 'bold', whiteSpace:'pre-line', textAlign:'center'}}>VALKYRIE{"\n"}
-                <p style={{ color:'#92989B', fontWeight: 'normal', whiteSpace:'pre-line', textAlign:'center', marginTop:'5px'}}>Welcome to the Pharmacy Portal</p>
+                <p style={{ color:'#FFFFFF', fontWeight: 'normal', whiteSpace:'pre-line', textAlign:'center'}}>Welcome to the Pharmacy Portal!</p>
                 </p>
             </div>
                 <img className='image' src={require('./../logo.png')}></img>
