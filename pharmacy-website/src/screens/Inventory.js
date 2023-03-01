40d1e3e86c149a0131906ac26fcb721b551d3864
adurasno <durasno.ayleen@gmail.com>
2023-03-01 01:22:08 -0500

    Updated inventory UI- still need to fix product_type styling

--- pharmacy-website/src/screens/Inventory.js
+++ pharmacy-website/src/screens/Inventory.js
@@ -2,18 +2,48 @@ import './Inventory.css';
 import { getInventoriesByPartitionKey } from './../services/ListInventories';
 import { useState, useEffect } from 'react';
 import { addInventoryItem } from './../services/addInventory';
+import Select from "react-dropdown-select";
 
 function Inventory() {
     const [inventories, setInventories] = useState([]);
+    const [name, SetName] = useState("");
+    const [price, SetPrice] = useState(0);
+    const [inventory, SetInventory] = useState(0);
+    const [weight, SetWeight] = useState(0);
+    const [type, SetType] = useState("");
+    const [selectedType, SetSelectedType] = useState("");
+    const itemTypes = [
+      {
+        id:1,
+        name:"Prescription"
+      },
+      {
+        id:2,
+        name:"Cold and Flu"
+      },
+      {
+        id:3,
+        name:"OTC" 
+      },
+      {
+        id:4,
+        name:"Vitamin and Immune Support" 
+      },
+      {
+        id:4,
+        name:"Misc." 
+      },
+    ];
     useEffect(() => {
         async function fetchInventories() {
           try {
-            const filter = 'Cold and Flu'; 
+            const filter = 'Testing'; 
             const items = await getInventoriesByPartitionKey(filter);
             setInventories(items);
             console.log(items);
           } catch (err) {
             console.log('Error fetching inventories', err);
+            setInventories([]);
           }
         }
         fetchInventories();
@@ -23,17 +53,19 @@ function Inventory() {
         console.log(inventories)
       }
 
-      async function addInventory(){
+      async function addInventory(name,price,inventory, weight, type ){
         try{
           console.log('add')
+
             const input = {
-                product_inventory: 10,
-                product_name: 'something',
-                product_type: 'some type',
-                product_price: 24.1,
-                product_weight: 1.3,
+                product_name: name,
+                product_price: parseFloat(price),
+                product_inventory:parseInt(inventory),
+                product_weight: parseFloat(weight),
+                product_type: type,
             };
             addInventoryItem(input);
+            console.log('added item');
         } catch (err) {
             console.log('Error adding to inventory', err);
         }
@@ -45,14 +77,56 @@ function Inventory() {
               <p style={{ color:'#92989B', fontWeight: 'bold', whiteSpace:'pre-line', textAlign:'center'}}>VALKYRIE{"\n"}
               </p>
             </div>
-            <p style={{color:'#FFFFFF', alignSelf:'flex-start' , marginLeft: 20,fontSize: 'calc(35px + 2vmin)', fontWeight:'bold'}}>Inventory</p>
+            <p style={{color:'#FFFFFF', alignSelf:'flex-start' , marginLeft: 20,fontSize: 'calc(25px + 2vmin)', fontWeight:'bold', marginTop:30, marginBottom:15}}>Inventory</p>
             <hr style={{border:'1px solid white', width: '98%'}}></hr>
-            <div>
+            <table className='tableWrapper'>
+            <tbody>
+      <tr key={"header"}>
+        <th className='th'>Product Name</th>
+        <th className='th'>Product Price</th>
+        <th className='th'> Product Inventory</th>
+        <th className='th'>Product Weight</th>
+        <th className='th'>Product Type</th>
+      </tr>
+      {inventories.map((item) => (
+        <tr key={item.id}>
+          <td className='td'>{item['product_name']}</td>
+          <td className='td'>{item['product_price']}</td>
+          <td className='td'>{item['product_inventory']}</td>
+          <td className='td'>{item['product_weight']}</td>
+          <td className='td'>{item['product_type']}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+    <form>
+  <label className='label'>
+    Product Name:
+    <input type="text" id="product_name" onChange={e => SetName(e.target.value)} required />
+  </label>
+  <label className='label'>
+    Product Price:
+    <input type="number" step="0.01" id="product_price" onChange={e => SetPrice(e.target.value)} required />
+  </label >
+  <label className='label'>
+    Product Inventory:
+    <input type="number" id="product_inventory" onChange={e => SetInventory(e.target.value)} required />
+  </label >
+  <label className='label'>
+    Product Weight:
+    <input type="number" step="0.01" id="product_weight" onChange={e => SetWeight(e.target.value)} required/>
+  </label>
+  <label className='label'>
+    Product Type:
+    <input type="text" id="product_type" onChange={e => SetType(e.target.value)} required />
+  </label>
+  <br></br>
+  <input type="Submit" value="Add" 
+      onClick={
+        addInventory(name, price, inventory, weight, type)
 
-            </div>
-            <img className='image' src={require('./../logo.png')}></img>
-            <button onClick={checkInventory}>check</button>
-            <button onClick={addInventory}>add</button>
+      }/>
+</form>
         </div>
     );
 }
