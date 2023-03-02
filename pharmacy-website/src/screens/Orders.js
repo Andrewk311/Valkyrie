af87f74cf86f7558091f397e635a8a0d16460526
Andrewk311 <Andrewking0619@gmail.com>
2023-03-02 02:13:11 -0500

    added up to see whatever order number you want. Can change filter to search email and status as well

--- pharmacy-website/src/screens/Orders.js
+++ pharmacy-website/src/screens/Orders.js
@@ -1,6 +1,31 @@
 import './Orders.css';
+import { getOrdersByNumber } from './../services/ListOrders';
+import { useEffect, useState } from 'react';
 
 function Orders() {
+
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        async function fetchOrder() {
+          try {
+            const filter = "9"; 
+            const items = await getOrdersByNumber(filter);
+            setOrders(items);
+            // console.log(items);
+          } catch (err) {
+            console.log('Error fetching orders', err);
+            setOrders([]);
+          }
+        }
+        fetchOrder();
+      }, []);
+
+    function handleShowOrderClick(){
+        console.log(orders)
+    }
+
+
     return (
         <div className="App">
             <div className='App-header'>
@@ -8,6 +33,7 @@ function Orders() {
                 <p style={{ color:'#92989B', fontWeight: 'normal', whiteSpace:'pre-line', textAlign:'center', marginTop:'5px'}}>ORDERS</p>
                 </p>
             </div>
+            <button onClick={handleShowOrderClick}>Show Order</button>
             <img className='image' src={require('./../logo.png')}></img>
         </div>
     );
---
+++ pharmacy-website/src/services/ListOrders.js
@@ -0,0 +1,48 @@
+import { API } from 'aws-amplify';
+
+export const LIST_ORDERS_QUERY = `
+  query listValkyrieOrders($filter: TableValkyrieOrdersFilterInput) {
+    listValkyrieOrders(filter: $filter) {
+        items {
+            email
+            isActive
+            location {
+                latitude
+                longitude
+            }
+            orderSpecification {
+                totalPrice
+                totalWeight
+            }
+            order_number
+            orders {
+                name
+                quantity
+            }
+        }
+    }
+  }
+`;
+
+export const getOrdersByNumber = async (orderNumber) => {
+    console.log('plz work 1')
+  try {
+    const filter = {
+        order_number: {
+            eq: orderNumber,
+        }
+    }
+    console.log('plz work')
+    const result = await API.graphql({
+      query: LIST_ORDERS_QUERY,
+      variables: {
+        filter
+      }
+    });
+    console.log('happens before this prob')
+    return result.data.listValkyrieOrders.items;
+  } catch (err) {
+    console.log('Error fetching inventories', err);
+    return [];
+  }
+};
