aba12f3e58bc094dea498bfe141fb199c9860e79
Andrewk311 <Andrewking0619@gmail.com>
2023-03-05 19:20:32 -0500

    amplify bug fix

---
+++ app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,33 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "valkyrie": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "valkyrie9d01b9df": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "CreatedSNSRole": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "valkyrie": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
+    }
+  }
+}
