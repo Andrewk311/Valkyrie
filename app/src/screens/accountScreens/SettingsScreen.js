15ad4a9298d0033fcc77acfb93d79b4555190379
Andrewk311 <Andrewking0619@gmail.com>
2023-04-02 22:47:42 -0400

    changed the address entry box so the user can edit their address instead of retyping it. Also added 2 custom auth fields for lat and lng so the calculation of the coords happens on that page instead of in the cart page

--- app/src/screens/accountScreens/SettingsScreen.js
+++ app/src/screens/accountScreens/SettingsScreen.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
-import { ScrollView, StyleSheet } from 'react-native';
+import { ScrollView, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import { Ionicons } from "@expo/vector-icons";
 import { Authenticator, useAuthenticator} from '@aws-amplify/ui-react-native';
 import { Auth } from 'aws-amplify';
 import react from "react";
 import { TextInput } from "react-native-paper";
 import { TouchableOpacity } from "react-native";
+import Geocoder from 'react-native-geocoding';
+
   
 const Settings = (props) => {
   const [attributes, setAttributes] = React.useState(null);
   const [inputAddress, setInputAddress] = React.useState('');
   const [hiddenText, setHiddenText] = React.useState('');
+  const [name, setName] = React.useState('');
+  const [familyName, setFamilyName] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [address, setAddress] = React.useState('');
+  const [refreshKey, setRefreshKey] = React.useState(0);
+
+  var googleMapsAPI = "AIzaSyBmF_o7JmYo55KiewrTHqiDOupJ5FcbxRA";   //google maps api key to convert address to lat and long
+  Geocoder.init(googleMapsAPI);
 
   // first retrival of user info
   React.useEffect(() => {
@@ -23,25 +33,48 @@ const Settings = (props) => {
   }, []);
 
 
-  var name = ''
-  var family_name = ''
-  var email = ''
-  var address = ''
-  if(attributes == null){
-  } else {
-    name = attributes.attributes.name.toString();
-    family_name = attributes.attributes.family_name.toString();
-    email = attributes.attributes.email.toString();
-    if(attributes.attributes.address != null){
-      address = attributes.attributes.address.toString();
+  async function getCoords(address) {
+    console.log("address for G: ", address);
+    try {
+      const json = await Geocoder.from(address);
+      const { lat, lng } = json.results[0].geometry.location;
+      console.log("lng: ", lng);
+      return { latitude: lat, longitude: lng };
+    } catch (error) {
+      console.warn(error);
     }
   }
+
+  // var name = ''
+  // var family_name = ''
+  // var email = ''
+  // var address = ''
+  React.useEffect(() => {
+    if (attributes) {
+      console.log(attributes)
+      setName(attributes.attributes.name.toString());
+      setFamilyName(attributes.attributes.family_name.toString());
+      setEmail(attributes.attributes.email.toString());
+      if (attributes.attributes.address != null) {
+        setAddress(attributes.attributes.address.toString());
+        setInputAddress(attributes.attributes.address.toString());
+      }
+    }
+  }, [attributes]);
+  
+  
   
   // update address 
   async function updateUserInfo(inputAddress) {
+    const coords = await getCoords(inputAddress);
+    const { latitude: lat, longitude: lng } = coords;
+    console.log('lat: ', lat);
+    console.log(inputAddress)
     const user = await Auth.currentAuthenticatedUser();
       await Auth.updateUserAttributes(user, {
-        'address': inputAddress
+        'address': inputAddress,
+        'custom:latitude': lat.toString(),
+        'custom:longitude': lng.toString()
       });
     setAttributes(user);
   }
@@ -53,6 +86,7 @@ const Settings = (props) => {
     else{
       setHiddenText('Address Updated');
       await updateUserInfo(inputAddress)
+      setRefreshKey(refreshKey + 1);
     }
     
   };
@@ -62,50 +96,52 @@ const Settings = (props) => {
     <Authenticator.Provider>
       <Authenticator>
         <ScrollView style={{marginHorizontal:0, backgroundColor:"rgba(227,55,55,1)"}}>
-        <View style={{ flex: 1, justifyContent: "center" }}>
-          <Text style={styles.whiteTitle}>General Info</Text>
-          <View style={{ borderBottomColor: 'white', borderBottomWidth: 3, width:'100%', alignSelf:'center', marginBottom:20,}}/>
-          <View style={{marginLeft:20,}}>
-          <Text style={styles.header}>Full Name</Text>
-            <TextInput
-              style={styles.input}
-              value={name + ' ' + family_name}
-              editable={false}
-            />
-          <Text style={styles.header}>Email Address</Text>
-            <TextInput
-              style={styles.input}
-              value={email}
-              editable={false}
-            />
-          <Text style={styles.header}>Delivery Address</Text>
-          {address != '' ? 
-          <TextInput 
-            style={styles.input} 
-            onChangeText={setInputAddress} 
-            value={address} 
-            editable={false}/> : <TextInput 
-            style={styles.input} 
-            onChangeText={setInputAddress} 
-            value={"Missing Address, Please input Below"} 
-            editable={false}/>}
-          <Text style={styles.header}>Enter a Delivery Address</Text>
-          <View style={{flexDirection: 'column', marginBottom:10}}>
-            <TextInput
-              style={styles.input}
-              onChangeText={setInputAddress}
-              value={inputAddress}
-              multiline
-            />
-          <TouchableOpacity
-                onPress={handleSubmit}
-                style= {styles.btn}>
-                <Text style={{fontSize:18, fontWeight:'bold'}}>Enter</Text>
-            </TouchableOpacity>
-          </View>     
-        <Text style={{fontSize:18, fontWeight:'bold', textAlign:'center'}}>{hiddenText}</Text>
-        </View>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+          <View style={{ flex: 1, justifyContent: "center" }}>
+            <Text style={styles.whiteTitle}>General Info</Text>
+            <View style={{ borderBottomColor: 'white', borderBottomWidth: 3, width:'100%', alignSelf:'center', marginBottom:20,}}/>
+            <View style={{marginLeft:20,}}>
+            <Text style={styles.header}>Full Name</Text>
+              <TextInput
+                style={styles.input}
+                value={name + ' ' + familyName}
+                editable={false}
+              />
+            <Text style={styles.header}>Email Address</Text>
+              <TextInput
+                style={styles.input}
+                value={email}
+                editable={false}
+              />
+            <Text style={styles.header}>Delivery Address</Text>
+            {address != '' ? 
+            <TextInput 
+              style={styles.input} 
+              onChangeText={setInputAddress} 
+              value={inputAddress} 
+              editable={true}/> : <TextInput 
+              style={styles.input} 
+              onChangeText={setInputAddress} 
+              value={"Missing Address, Please input Below"} 
+              editable={false}/>}
+            {/* <Text style={styles.header}>Enter a Delivery Address</Text> */}
+            <View style={{flexDirection: 'column', marginBottom:10}}>
+              {/* <TextInput
+                style={styles.input}
+                onChangeText={setInputAddress}
+                value={inputAddress}
+                multiline
+              /> */}
+            <TouchableOpacity
+                  onPress={handleSubmit}
+                  style= {styles.btn}>
+                  <Text style={{fontSize:18, fontWeight:'bold'}}>Enter</Text>
+              </TouchableOpacity>
+            </View>     
+          <Text style={{fontSize:18, fontWeight:'bold', textAlign:'center'}}>{hiddenText}</Text>
+          </View>
+          </View>
+          </TouchableWithoutFeedback>
         </ScrollView>
       </Authenticator>
     </Authenticator.Provider>
