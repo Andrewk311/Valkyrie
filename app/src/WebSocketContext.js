6c952513c4fe6685fcf54f6af3a16d57cb88fd15
Andrewk311 <Andrewking0619@gmail.com>
2023-04-04 17:41:08 -0400

    updates connection table with email of user

--- app/src/WebSocketContext.js
+++ app/src/WebSocketContext.js
@@ -1,17 +1,38 @@
 import React, { createContext, useState, useEffect } from 'react';
+import { Auth } from 'aws-amplify';
 
 export const WebSocketContext = createContext();
 
 export const WebSocketProvider = ({ children }) => {
   const [websocket, setWebsocket] = useState(null);
   const [orderStatus, setOrderStatus] = useState(null); // Add orderStatus state
+  const [email, setEmail] = useState(null);
 
   useEffect(() => {
+    async function getUserInfo() {
+      const user = await Auth.currentAuthenticatedUser();
+      setEmail(user.attributes.email);
+    }
+    getUserInfo();
+  }, []);
+
+  useEffect(() => {
+
+    if(!email){
+      return;
+    }
+
     const ws = new WebSocket("wss://07k3svmpdh.execute-api.us-east-1.amazonaws.com/production");
 
     ws.onopen = () => {
       console.log("Connected to WebSocket");
       setWebsocket(ws);
+      console.log(email)
+
+      setTimeout(() => {
+        console.log("Sending email message");
+        ws.send(JSON.stringify({ type: 'email', email: email }));
+      }, 10000); // Add a delay of 1 second before sending the message
     };
 
     ws.onmessage = (event) => {
@@ -43,14 +64,14 @@ export const WebSocketProvider = ({ children }) => {
       } else {
         console.log("Skipping ping")
       }
-    }, 40000); // Ping every 20 seconds
+    }, 60000);
 
     return () => {
       if (ws) {
         ws.close();
       }
     }; 
-  }, []);
+  }, [email]);
 
   return (
     <WebSocketContext.Provider value={{ websocket, setWebsocket, orderStatus, setOrderStatus }}>
