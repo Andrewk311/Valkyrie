5faf3e41c1092c7f3fb433dbadb4bea1fcc811d3
Andrewk311 <Andrewking0619@gmail.com>
2023-04-11 18:11:58 -0400

    started adding skeleton for content in how its built page. Made it so you can choose which feature to learn about instead of showing it all at once and making the user scroll.

--- website/src/screens/How.js
+++ website/src/screens/How.js
@@ -1,19 +1,23 @@
 import './How.css';
 import { useNavigate } from 'react-router-dom';
-import appImage from './../Images/HomePage.PNG';
+import appImage from './../Images/Software.png';
 import droneImage from './../Images/Drone.jpg';
 import droneBoxImage from './../Images/Dallas5.JPG';
 import React, { useRef, useState, useEffect } from 'react';
+import appSignIn from './../Images/AppSignIn.png';
+import appSignUp from './../Images/AppSignUp.png';
+
 
 function How() {
 
     const navigate = useNavigate();
     const [selectedCategory, setSelectedCategory] = useState(null);
     const topRef = useRef(null);
+    const [selectedFeature, setSelectedFeature] = useState(null);
 
     useEffect(() => {
         topRef.current.scrollIntoView();
-      }, []);
+    }, []);
 
     function handleAboutUsClick() {
         navigate('/AboutUs')
@@ -25,7 +29,12 @@ function How() {
 
     function handleCategoryClick(category) {
         setSelectedCategory(category);
-      }
+    }
+
+    function handleFeatureClick(feature) {
+        setSelectedFeature(feature);
+    }
+
 
     return (
         <div className="HowBuilt">
@@ -33,7 +42,7 @@ function How() {
             <header className="howBuiltHeader">
                 <button className="button-header button-margin-howBuilt" onClick={handleHomeClick}>
                     Go Back
-            </button>
+                </button>
                 <p className="header-text">VALKYRIE CAPSTONE GROUP 19</p>
             </header>
             <section className="howBuiltSection">
@@ -45,7 +54,7 @@ function How() {
                         <img className="category-image-app" src={appImage} alt="App" />
                         <p className="category-description">
                             App/Pharmacy Portal
-                </p>
+                        </p>
                     </div>
 
                     <div className="category" onClick={() => handleCategoryClick({ title: 'The Drone' })}>
@@ -53,7 +62,7 @@ function How() {
                         <img className="category-image-drone" src={droneImage} alt="Drone" />
                         <p className="category-description">
                             Drone
-                </p>
+                        </p>
                     </div>
 
                     <div className="category" onClick={() => handleCategoryClick({ title: 'Drone Box System' })}>
@@ -61,17 +70,116 @@ function How() {
                         <img className="category-image-box" src={droneBoxImage} alt="Drone Box" />
                         <p className="category-description">
                             Payload System
-                </p>
+                        </p>
                     </div>
                 </div>
                 {selectedCategory && (
                     <div className="selected-category-info">
                         <h2>{selectedCategory.title} Features:</h2>
-                        {/* Render your features here */}
+                        <div className="features-and-tabs">
+                            {selectedCategory.title === 'App/Pharmacy Portal' && (
+                                <div>
+                                    <div className="feature-tabs">
+                                        <button onClick={() => handleFeatureClick("signup")}>Sign-up/Login</button>
+                                        <button onClick={() => handleFeatureClick("delivery")}>Delivery Address Confirmation</button>
+                                        <button onClick={() => handleFeatureClick("product")}>Product Screens/Cart</button>
+                                        <button onClick={() => handleFeatureClick("orderStatus")}>Order Status Updates</button>
+                                        <button onClick={() => handleFeatureClick("tracking")}>Tracking</button>
+                                    </div>
+                                    {/* App/Pharmacy Portal content */}
+                                    {selectedFeature === "signup" && (
+                                        <div className="feature-section">
+                                            <h3>Sign-up/Login:</h3>
+                                            <div className="feature-content">
+                                                <div className="image-container">
+                                                    <img className="feature-image whiteAppImages" src={appSignIn} alt="Image 1" />
+                                                    <img className="feature-image whiteAppImages" src={appSignUp} alt="Image 2" />
+                                                </div>
+                                                <p className="feature-description">
+                                                    {/* Add the text description here */}
+                                                    This is a description of the Sign-up/Login feature.
+                                                </p>
+                                            </div>
+                                        </div>
+                                    )}
+
+                            {selectedFeature === "delivery" && (
+                                <div className="feature-section">
+                                    {/* Delivery Address Confirmation content */}
+                                </div>
+                            )}
+
+                            {selectedFeature === "product" && (
+                                <div className="feature-section">
+                                    {/* Product Screens/Cart content */}
+                                </div>
+                            )}
+
+                            {selectedFeature === "orderStatus" && (
+                                <div className="feature-section">
+                                    {/* Order Status Updates content */}
+                                </div>
+                            )}
+
+                            {selectedFeature === "tracking" && (
+                                <div className="feature-section">
+                                    {/* Tracking content */}
+                                </div>
+                            )}
+                        </div>
+                            )}
+                    </div>
+                        {selectedCategory.title === 'The Drone' && (
+                    <div>
+                        <div>
+                            <button onClick={() => handleFeatureClick("Parts")}>Parts</button>
+                            <button onClick={() => handleFeatureClick("Battery")}>Battery</button>
+                            <button onClick={() => handleFeatureClick("Autonomous Flight")}>Autonomous Flight</button>
+                        </div>
+                        {/* App/Pharmacy Portal content */}
+                        {selectedFeature === "Parts" && (
+                            <div className="feature-section">
+                                {/* Sign-up/Login content */}
+                            </div>
+                        )}
+
+                        {selectedFeature === "Battery" && (
+                            <div className="feature-section">
+                                {/* Delivery Address Confirmation content */}
+                            </div>
+                        )}
+
+                        {selectedFeature === "Autonomous Flight" && (
+                            <div className="feature-section">
+                                {/* Product Screens/Cart content */}
+                            </div>
+                        )}
                     </div>
                 )}
-            </section>
+                {selectedCategory.title === 'Drone Box System' && (
+                    <div>
+                        <div>
+                            <button onClick={() => handleFeatureClick("Box")}>Box</button>
+                            <button onClick={() => handleFeatureClick("Electrical")}>Electrical Components</button>
+                        </div>
+                        {/* App/Pharmacy Portal content */}
+                        {selectedFeature === "Box" && (
+                            <div className="feature-section">
+                                {/* Sign-up/Login content */}
+                            </div>
+                        )}
+                        {selectedFeature === "Electrical" && (
+                            <div className="feature-section">
+                                {/* Delivery Address Confirmation content */}
+                            </div>
+                        )}
+                    </div>
+                )}
         </div>
+    )
+}
+            </section >
+        </div >
     );
 }
 
